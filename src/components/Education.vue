c3b482561bb00552aa151e0bbdfc9a8afaa7c696
Cully Mason <cullymason@Cullys-MacBook-Pro.local>
2020-01-14 21:17:20 -0500

    fixed reference

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,17 +1,20 @@
  <template>
   <section>
+      
     <h2
       class="text-lg md:text-xl uppercase font-black text-orange-500 tracking-wider mt-2 mb-2"
     >Education</h2>
-    <h3 v-for="school in schools" :key="school" class="flex flex-col md:flex-row mb-1 pb-1">
+    <article v-for="school in schools" :key="school">
+    <h3 class="flex flex-col md:flex-row mb-1 pb-1">
       <span class="font-bold text-gray-900 pr-1">{{school.name}},</span>
       <span class="pr-2 text-gray-800">{{school.degree}}</span>
       <span class="text-gray-600">{{school.startYear}}-{{school.endYear}}</span>
     </h3>
-    <small class="text-gray-700 italic mb-2"> Completed credits towards a degree in Computer Science </small>
+    <small v-if="school.disclaimer" class="text-gray-700 italic mb-2"> {{school.disclaimer}} </small>
     <ul v-for="accomplishment in school.accomplishments" :key="accomplishment" class="list-disc list-outside mt-2">
       <li> {{accomplishment}} </li>
     </ul>
+    </article>
   </section>
 </template>
  
